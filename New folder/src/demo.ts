a80183b57170e7536316f08e1758787f991e253c
Rahul <Rahul@DESKTOP-ILN9P4E>
2021-07-08 19:45:03 +0530

    Working Skeleton

---
+++ New folder/src/demo.ts
@@ -0,0 +1,21 @@
+﻿export interface Duck {
+    name: string;
+    numLegs: number;
+    makeSound: (sound: string) => void;
+}
+
+const duck1: Duck = {
+    name: 'huey',
+    numLegs: 2,
+    makeSound: (sound: any) => console.log(sound)
+}
+
+const duck2: Duck = {
+    name: 'dwey',
+    numLegs: 2,
+    makeSound: (sound: string)=> console.log(sound)
+}
+
+duck1.makeSound('quack');
+
+export const ducks = [duck1, duck2]
